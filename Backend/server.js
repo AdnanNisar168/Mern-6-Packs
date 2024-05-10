758fb614be7f90e66d5aa09bd6c2651bbd88e6b9
adnan <adnan.nisar@quicktijarat.com>
2024-05-10 21:32:54 +0500

    port issue resolved

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,16 @@
 const { Console } = require("console");
 const app = require("./app");
-
+port = 3200
 //telling port that, it is env file
 const dotenv = require("dotenv");
 
 //config
-dotenv.config({path:"backend/config/conig.env"});
+dotenv.config({path:"backend/config/config.env"});
 
 //listening server
 app.listen(process.env.PORT, ()=>{
-    Console.log(`server is working on http://localhost:${process.env.PORT}`);
+    console.log(`server is working on http://localhost:${process.env.PORT}`);
 })
+// app.listen(port, ()=>{
+//     console.log(`server is working on http://localhost:${port}`);
+// })
