55985f04bd9e2d7cf5be5db8281adf13633bab37
AdnanNisar168 <adnannisar467@gmail.com>
2024-11-27 09:33:37 +0500

    commiting files from pc

--- Backend/server.js
+++ Backend/server.js
@@ -4,8 +4,14 @@ port = 3200
 //telling port that, it is env file
 const dotenv = require("dotenv");
 
+const connectDataBase = require("./config/database");
+
 //config
 dotenv.config({path:"backend/config/config.env"});
+//connect DataBase
+connectDataBase();
+
+
 
 //listening server
 app.listen(process.env.PORT, ()=>{
